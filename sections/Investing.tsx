c9481ab40e7d222401a2e5e5dbc19097e32932a4
Umidjon Vokhidov <umidjonvokhidov.uz@gmail.com>
2025-05-20 11:09:01 +0500

    Investing in your future is crucial section finished

--- sections/Investing.tsx
+++ sections/Investing.tsx
@@ -1,5 +1,69 @@
+import Image from 'next/image';
+
 const Investing = () => {
-  return <div>Investing</div>;
+  return (
+    <section className="container-main container-spacing-main">
+      <div className="flex-col-center container gap-y-20 max-lg:gap-y-16 max-md:gap-y-8">
+        <div className="flex-col-center w-full gap-y-4 max-lg:gap-y-3">
+          <div className="border-grey-100 bg-background flex items-center gap-x-2 rounded-3xl border px-3 py-2">
+            <Image
+              src="/assets/icons/zap-fast.svg"
+              width={24}
+              height={24}
+              alt="zap-fast"
+            />
+            <span className="text-grey-600 text-center text-sm font-medium">
+              Investing in your future is crucial.
+            </span>
+          </div>
+          <h1 className="h-1_5 w-[700px] text-center max-lg:w-full">
+            Our team is constantly innovating.
+          </h1>
+        </div>
+        <div className="grid grid-cols-2 gap-12 max-md:grid-cols-1">
+          <div className="flex-col-center col-span-1 gap-8 rounded-3xl bg-[#181D27] px-8 py-10 max-lg:gap-6 max-lg:px-6 max-lg:py-8">
+            <div className="flex-col-center gap-y-6 max-lg:gap-y-4">
+              <h6 className="text-center text-[#FAFAFA]">
+                Real-Time Analytics & Reporting
+              </h6>
+              <p className="text-center text-[#FAFAFA]">
+                Gain full visibility of your finances with interactive
+                dashboards and reports. Make data-driven decisions with ease.
+              </p>
+            </div>
+            <Image
+              src="/assets/images/analytics-reporting-chart.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full object-contain object-bottom"
+              alt="Real-Time Analytics & Reporting"
+            />
+          </div>
+          <div className="flex-col-center col-span-1 gap-8 rounded-3xl bg-[#BCFC6A] px-8 py-10 max-lg:gap-6 max-lg:px-6 max-lg:py-8">
+            <div className="flex-col-center gap-y-6 max-lg:gap-y-4">
+              <h6 className="text-grey-800 text-center">
+                AI-Powered Budgeting Tools
+              </h6>
+              <p className="text-grey-800 text-center">
+                Track your spending and savings automatically. Our AI tools
+                provide personalized insights to help you stay on top of your
+                finances.
+              </p>
+            </div>
+            <Image
+              src="/assets/images/phone-credit-card.png"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full object-contain object-bottom"
+              alt="Real-Time Analytics & Reporting"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Investing;
--- sections/OurFeatures.tsx
+++ sections/OurFeatures.tsx
@@ -12,84 +12,87 @@ const OurFeatures = () => {
             your financial experience.
           </p>
         </div>
-        <div className="flex-col-center w-full gap-8">
-          <div className="grid w-full grid-cols-5 justify-stretch gap-8 max-lg:grid-cols-1 max-md:flex max-md:flex-col">
-            <div className="white-card flex items-center gap-8 max-md:flex-col">
-              <div className="col-span-3 flex flex-col items-start gap-y-6 max-md:items-center">
-                <h6 className="max-md:text-center">Real-time sync</h6>
-                <p className="max-md:text-center">
-                  Instantly sync your transactions and balances across all
-                  devices. Always stay updated with your latest financial data.
-                </p>
+        <div className="flex-col-center w-full gap-y-16">
+          <div className="flex-col-center w-full gap-8">
+            <div className="grid w-full grid-cols-5 justify-stretch gap-8  max-lg:flex max-lg:flex-col">
+              <div className="white-card flex items-center gap-8 max-md:flex-col">
+                <div className="col-span-3 flex flex-col items-start gap-y-6 max-md:items-center">
+                  <h6 className="max-md:text-center">Real-time sync</h6>
+                  <p className="max-md:text-center">
+                    Instantly sync your transactions and balances across all
+                    devices. Always stay updated with your latest financial
+                    data.
+                  </p>
+                </div>
+                <Image
+                  src="/assets/images/real-time-sync-chart.png"
+                  width={238}
+                  height={230}
+                  alt="realtime sync chart"
+                />
               </div>
-              <Image
-                src="/assets/images/real-time-sync-chart.png"
-                width={238}
-                height={230}
-                alt="realtime sync chart"
-              />
+              <div className="white-card col-span-2 flex w-full flex-col items-start gap-8 max-lg:flex-row max-md:flex-col">
+                <div className="flex flex-col gap-y-4">
+                  <h6 className="max-md:text-center">AI Budget Tools</h6>
+                  <p className="max-md:text-center">
+                    Track your spending and savings with our AI tools for
+                    personalized insights.
+                  </p>
+                </div>
+                <Image
+                  src="/assets/images/ai-budget-tools-chart.png"
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  className="h-auto w-full"
+                  alt="ai budget tools chart"
+                />
+              </div>
             </div>
-            <div className="white-card col-span-2 flex w-full flex-col items-start gap-8 max-lg:flex-row max-md:flex-col">
-              <div className="flex flex-col gap-y-4">
-                <h6 className="max-md:text-center">AI Budget Tools</h6>
-                <p className="max-md:text-center">
-                  Track your spending and savings with our AI tools for
-                  personalized insights.
-                </p>
+            <div className="grid w-full grid-cols-2 gap-8 max-lg:grid-cols-1">
+              <div className="white-card flex-col-center relative col-span-1 gap-y-[60px] overflow-x-hidden pb-[260px]">
+                <div className="flex flex-col items-center justify-start gap-y-4">
+                  <h6 className="max-md:text-center">Built-in integrations</h6>
+                  <p className="w-[400px] text-center max-md:w-full">
+                    Manage all your bank accounts in one dashboard for easier
+                    finances.
+                  </p>
+                </div>
+                <Image
+                  src="/assets/images/integrations.svg"
+                  width={796}
+                  height={152}
+                  className="absolute bottom-[60px] left-1/2 h-auto min-w-[796px] max-md:min-w-[600px] -translate-x-1/2"
+                  alt="ai budget tools chart"
+                />
               </div>
-              <Image
-                src="/assets/images/ai-budget-tools-chart.png"
-                width={0}
-                height={0}
-                sizes="100vw"
-                className="h-auto w-full"
-                alt="ai budget tools chart"
-              />
+              <div className="white-card flex-col-center col-span-1 gap-y-[60px] max-md:gap-y-7">
+                <Image
+                  src="/assets/images/qr-code-scan.png"
+                  width={373}
+                  height={180}
+                  sizes="100vw"
+                  className=""
+                  alt="qr code scan"
+                />
+                <div className="flex flex-col items-center justify-start gap-y-4">
+                  <h6 className="max-md:text-center">Payment Solutions</h6>
+                  <p className="w-[400px] text-center max-md:w-full">
+                    Easily connect globally. MezPay supports 100+ countries with
+                    multi-currency options.
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="grid w-full grid-cols-2 gap-8 overflow-hidden max-lg:grid-cols-1">
-            <div className="white-card flex-col-center relative col-span-1 gap-y-[60px] overflow-x-hidden pb-[260px]">
-              <div className="flex flex-col items-center justify-start gap-y-4">
-                <h6 className="max-md:text-center">Built-in integrations</h6>
-                <p className="w-[400px] text-center max-md:w-full">
-                  Manage all your bank accounts in one dashboard for easier
-                  finances.
-                </p>
-              </div>
-              <Image
-                src="/assets/images/integrations.svg"
-                width={796}
-                height={152}
-                className="absolute bottom-[60px] left-0 right-0 w-[796px] h-auto"
-                alt="ai budget tools chart"
-              />
-            </div>
-            <div className="white-card flex-col-center col-span-1 gap-y-[60px] max-md:gap-y-7">
-              <Image
-                src="/assets/images/qr-code-scan.png"
-                width={373}
-                height={180}
-                sizes="100vw"
-                className=""
-                alt="qr code scan"
-              />
-              <div className="flex flex-col items-center justify-start gap-y-4">
-                <h6 className="max-md:text-center">Payment Solutions</h6>
-                <p className="w-[400px] text-center max-md:w-full">
-                  Easily connect globally. MezPay supports 100+ countries with
-                  multi-currency options.
-                </p>
-              </div>
-            </div>
+          <div className="flex justify-center gap-6 max-md:gap-4 max-sm:flex-col items-center">
+            <Button href="/contact" buttonType="secondary">
+              Contact Us
+            </Button>
+            <Button href="/pricing" buttonType="primary">
+              Pricing
+            </Button>
           </div>
-        </div>
-        <div className="flex justify-center gap-x-6">
-          <Button href="/contact" buttonType="secondary">
-            Contact Us
-          </Button>
-          <Button href="/pricing" buttonType="primary">
-            Pricing
-          </Button>
         </div>
       </div>
     </section>
