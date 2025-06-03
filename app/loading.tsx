e86e03e0e3be7c5b1a2001e210ae781d5b618004
Umidjon Vokhidov <umidjonvokhidov.uz@gmail.com>
2025-06-03 21:14:20 +0500

    loading updated

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+
 const loading = () => {
   return (
-    <div className="flex-col-center h-screen w-full text-xl">
-      Page Loading...
+    <div className="flex h-screen items-center justify-center bg-white">
+      <Image
+        src="/assets/icons/Logo.svg"
+        alt="Loading..."
+        width={201}
+        height={48}
+        className="animate-pulse-scale"
+      />
     </div>
   );
 };
 
 export default loading;
+
+
